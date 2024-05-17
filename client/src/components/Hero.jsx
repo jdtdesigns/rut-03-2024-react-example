cc074ee4841c47bdcbd71ef4ee8cd026004e1b7b
JD Tadlock <jt7903@gmail.com>
2024-05-17 12:50:41 -0400

    started outputting values

--- client/src/components/Hero.jsx
+++ client/src/components/Hero.jsx
@@ -1,7 +1,12 @@
 function Hero() {
+  const fullName = 'JD Tadlock'
+  const age = 44
+  const outputAge = false
+
   return (
     <section className="hero text-center">
-      <h1>JD Tadlock</h1>
+      <h1>{fullName}</h1>
+      {outputAge && <p>Age: {age}</p>}
       <p>Dev'ing Your Dreams</p>
     </section>
   )

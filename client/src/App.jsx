29354830c3052c76ddba4248367861cb7a992cd5
JD Tadlock <jt7903@gmail.com>
2024-05-21 13:33:44 -0400

    main base

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,24 +1,9 @@
 
-const styles = {
-  headerStyles: {
-    background: '#555',
-    color: '#eee'
-  }
-}
-
 function App() {
   return (
-    <main>
-      <header style={styles.headerStyles} className="row justify-between align-center">
-        <h3>Logo</h3>
-
-        <nav>
-          <a className="red" href="/">Home</a>
-          <a href="/about">About</a>
-          <a href="/contact">Contact</a>
-        </nav>
-      </header>
-    </main>
+    <>
+      <h1>Main Base</h1>
+    </>
   )
 }
 

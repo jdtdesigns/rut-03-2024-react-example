1eace7c68824f6f9c3759c34743ca0805efccc7e
JD Tadlock <jt7903@gmail.com>
2024-05-21 10:42:25 -0400

    finished activity 12

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,46 +1,9 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
+// First we import Greeting from the components folder
+import Greeting from "./components/Greeting";
 
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Modal from './components/Modal'
-
-import Landing from './pages/Landing'
-import About from './pages/About'
-import Contact from './pages/Contact'
-
+// Our App component returns the Greeting component
 function App() {
-  const [darkMode, setDarkMode] = useState(false)
-  const [showModal, setShowModal] = useState(false)
-
-  return (
-    <main className={darkMode ? 'dark' : ''}>
-
-      <Header
-        setDarkMode={setDarkMode}
-        setShowModal={setShowModal} />
-
-      <div className="content">
-        <Routes>
-          <Route path="/" element={<Landing />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-      </div>
-
-      <Footer />
-
-      {showModal && <Modal setShowModal={setShowModal} />}
-
-    </main>
-  )
+  return <Greeting />;
 }
 
-export default App
-
-
-
-
-
-
-
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,6 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.scss'
+import ReactDOM from 'react-dom/client';
+import App from './App';
+// Here we import bootstrap for access to some additional styling
+import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { BrowserRouter } from 'react-router-dom'
-
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>,
-)
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);

8a877711957bd259e953b554da0ac0cea9c6c529
JD Tadlock <jt7903@gmail.com>
2024-05-23 14:35:35 -0400

    really ready for deploy

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 
 const app = express()
 const PORT = process.env.PORT || 3333
@@ -14,6 +15,14 @@ app.use(express.json())
 app.use(cookieParser())
 
 app.use('/api/auth', auth_routes)
+
+if (process.env.PORT) {
+  app.use(express.static('../client/dist'))
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, '../client/dist/index.html'))
+  })
+}
 
 client.once('open', () => {
   app.listen(PORT, () => console.log('Server started on port', PORT))

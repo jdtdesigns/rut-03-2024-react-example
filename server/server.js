c65f2ea297b95e9fba8281ac4fc6c0b4ede41c8b
JD Tadlock <jt7903@gmail.com>
2024-05-23 09:52:35 -0400

    finished apollo test

--- server/server.js
+++ server/server.js
@@ -5,6 +5,24 @@ const PORT = process.env.PORT || 3333
 
 const client = require('./config/client')
 
+const { ApolloServer, gql } = require('apollo-server-express');
+
+// Construct a schema, using GraphQL schema language
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+// Provide resolver functions for your schema fields
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!',
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
 app.use(express.json())
 
 app.get('/api/test', (req, res) => {
@@ -13,7 +31,15 @@ app.get('/api/test', (req, res) => {
   })
 })
 
-client.once('open', () => {
-  app.listen(PORT, () => console.log('Server started on port', PORT))
+
+client.once('open', async () => {
+  await server.start()
+
+  server.applyMiddleware({ app });
+
+  app.listen(PORT, () => {
+    console.log('Server started on port', PORT)
+    console.log('GraphQL waiting at', server.graphqlPath)
+  })
 })
 
